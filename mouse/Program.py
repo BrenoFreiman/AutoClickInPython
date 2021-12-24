cab402a9aaa046b60554913ee87d6bfe36bd8ab5
BrenoFreiman <breninhshow@gmail.com>
2021-12-24 03:25:16 -0300

    CommitChanges

--- mouse/Program.py
+++ mouse/Program.py
@@ -1,6 +1,8 @@
 from Teste import AutoClick
 from threading import Thread
 
+AutoClick.Start()
+p2 = Thread(target = AutoClick.Close)
 p1 = Thread(target = AutoClick.AutoClickDef)
+p2.start()
 p1.start()
-
--- mouse/Teste.py
+++ mouse/Teste.py
@@ -1,12 +1,43 @@
+from enum import auto
+from re import DEBUG
 import mouse
 import time
 import keyboard
+from threading import Thread
 
 class AutoClick():
 
+    Speed = float(0)
+    Clicking = True
+    key = ""
+    def Start():
+        AutoClick.key = input("Choose a keyboard key to activate: ")
+        AutoClick.ChooseSpeed()
+
     def AutoClickDef():
-        while True:
-            if keyboard.is_pressed('b'):
+        while AutoClick.Clicking:
+            if AutoClick.Clicking & keyboard.is_pressed(AutoClick.key):
                 mouse.click('left')
-                print("Pressed")
-            time.sleep(0.01)
+            time.sleep(1/AutoClick.Speed)
+
+    def ChooseSpeed():
+        try:
+            AutoClick.Speed = float(input("clicks per second: "))
+        except:
+            print("Only numbers are acceptable")
+            AutoClick.ChooseSpeed()
+
+    def Close():
+        print("Press F to Pause and Unpause")
+        while True:
+            if keyboard.is_pressed("f"):
+                if AutoClick.Clicking == False:
+                    print("UnPaused")
+                    AutoClick.Start()
+                    AutoClick.Clicking = True
+                    time.sleep(1)
+                elif AutoClick.Clicking == True:
+                    print("Paused")
+                    AutoClick.Clicking = False
+                    time.sleep(1)
+            time.sleep(1/15)
